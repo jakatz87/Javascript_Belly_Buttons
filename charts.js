80b23512321cfbd896c34d88a9a929c7126978a4
Joshua Katz <jakatz87@gmail.com>
2023-01-09 17:30:15 -0500

    Update charts.js

--- charts.js
+++ charts.js
@@ -150,8 +150,8 @@ function buildCharts(sample) {
     ]
     // Deliverable 3: 5. Create the layout for the gauge chart.
     var layoutg = {
-      width: 400,
-      height: 350,
+      width: 450,
+      height: 400,
       margin: {t:0,b:0}
     };
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
